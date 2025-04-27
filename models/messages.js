6744eeda9ed53c2d9e10cf6a633c0904a0071e0f
Miles1744 <mithomtab@gmail.com>
2025-04-27 17:58:44 -0400

    New

---
+++ models/messages.js
@@ -0,0 +1,6 @@
+const messages = [
+    { user: "Amando", text: "Hi there!", added: new Date() },
+    { user: "Charles", text: "Hello world!", added: new Date() }
+  ];
+  
+  module.exports = messages;
