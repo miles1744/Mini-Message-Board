b64fff0fe68e079f211345c388788e5ea25c2a32
Miles1744 <mithomtab@gmail.com>
2025-04-27 15:55:32 -0400

    New

--- controllers/NewController.js
+++ controllers/NewController.js
@@ -1,11 +1,5 @@
 module.exports = {
     get: (req, res) => {
-        res.render("new", {
-            title: "Mini Message Board",
-            messages: [
-              { user: "Amando", text: "Hi there!", added: new Date() },
-              { user: "Charles", text: "Hello world", added: new Date() }
-            ]
-        });
+        res.render("new");
     }
 };
