b05e0791f7ff080a091dba4bc8002f1a87f72af3
Miles1744 <mithomtab@gmail.com>
2025-04-27 14:11:47 -0400

    New

--- routes/Index.js
+++ routes/Index.js
@@ -1,19 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
-  
 
 
 const controller = require("../controllers/IndexController")
