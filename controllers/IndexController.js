5b80a03e97f99f85d3d86b85fcaeadf61f764b2c
Miles1744 <mithomtab@gmail.com>
2025-05-06 06:59:09 -0400

    New

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,8 +1,8 @@
 const db = require("../db/messages");
 
 module.exports = {
-    get: (req, res) => {
-        const messages = db.getAllMessages()
+    get: async (req, res) => {
+        const messages = await db.getAllMessages()
         res.render("index",{
                 title: "Mini Message Board",
                 messages
